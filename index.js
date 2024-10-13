ca38db5f90f5774515e51614a37b301cdceb78ad
Mohit13333 <mohitkumar143680@gmail.com>
2024-10-14 03:34:57 +0530

    push

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { Server } from 'socket.io';
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
+app.use(cors());
 
 // Serve static files (client-side)
 app.use(express.static('src'));
