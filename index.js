d63a14fc3fa9f4fc30b66b764d126bdd2794085e
Mohit13333 <mohitkumar143680@gmail.com>
2024-10-14 14:04:58 +0530

    favicon

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 app.use(cors());
 
-// Serve static files (client-side)
-app.use(express.static('src'));
+app.use(express.static("src"));
 
-// Track active users
 let users = 0;
 
-io.on('connection', (socket) => {
-    // If users limit is reached, notify and disconnect
-    if (users >= 2) {
-        socket.emit('chatFull'); // Inform client that chat is full
-        socket.disconnect();     // Disconnect the socket
-        return;
-    }
+io.on("connection", (socket) => {
+  if (users >= 2) {
+    socket.emit("chatFull");
+    socket.disconnect();
+    return;
+  }
+  users++;
+  console.log(`User connected. Total users: ${users}`);
+  socket.broadcast.emit("userConnected", `A new user has joined the chat.`);
 
-    // Increment user count and broadcast message to other users
-    users++;
-    console.log(`User connected. Total users: ${users}`);
-    socket.broadcast.emit('userConnected', `A new user has joined the chat.`);
+  socket.on("chatMessage", (data) => {
+    io.emit("chatMessage", data);
+  });
 
-    // Handle chat messages
-    socket.on('chatMessage', (data) => {
-        io.emit('chatMessage', data); // Broadcast message to all clients
-    });
-
-    // Handle user disconnection
-    socket.on('disconnect', () => {
-        users--;
-        console.log(`User disconnected. Total users: ${users}`);
-        socket.broadcast.emit('userDisconnected',` A user has left the chat.`);
-    });
+  socket.on("disconnect", () => {
+    users--;
+    console.log(`User disconnected. Total users: ${users}`);
+    socket.broadcast.emit("userDisconnected", ` A user has left the chat.`);
+  });
 });
 
 server.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
